d20f15a18aa17243153e8d014df78a28f7f20f66
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-03 00:00:49 +0100

    feat: implement DeleteParameter

--- src/targets/DeleteParameter.ts
+++ src/targets/DeleteParameter.ts
@@ -2,16 +2,33 @@ import {
   DeleteParameterRequest,
   DeleteParameterResult,
 } from "aws-sdk/clients/ssm";
-import { ResourceNotFoundError } from "../errors";
+import { ParameterNotFound } from "../errors";
 import { Services } from "../services";
 import { Target } from "./router";
 
-export type DeleteParameterTarget = Target<DeleteParameterRequest, DeleteParameterResult>;
+export type DeleteParameterTarget = Target<
+  DeleteParameterRequest,
+  DeleteParameterResult
+>;
 
 export const DeleteParameter =
   ({ ssm }: Pick<Services, "ssm">): DeleteParameterTarget =>
   async (ctx, req) => {
-    ctx.logger.debug({Name: req.Name}, "DeleteParameter");
+    ctx.logger.debug({ Name: req.Name }, "DeleteParameter");
 
-    return await Promise.resolve({});
+    const param = await ssm.get(ctx, req.Name);
+
+    if (!param) {
+      throw new ParameterNotFound();
+    }
+
+    const success = await ssm.delete(ctx, req.Name);
+
+    if (!success) {
+      ctx.logger.error(
+        `The parameter '${req.Name}' has failed to delete from the database`
+      );
+    }
+
+    return {};
   };
