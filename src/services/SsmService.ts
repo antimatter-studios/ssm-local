b964d8de9e519e4264c05d3498f73fc29353dbbf
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-02 23:38:11 +0100

    fix: return true or false if the parameter exists after deletion

--- src/services/SsmService.ts
+++ src/services/SsmService.ts
@@ -6,12 +6,16 @@ export interface SsmParameterInterface {
   Value: string;
   Metadata: ParameterMetadata;
   TagList: TagList;
-};
+}
 
 export interface SsmServiceInterface {
-  all(ctx: Context): Promise<SsmParameterInterface[]>
+  all(ctx: Context): Promise<SsmParameterInterface[]>;
   get(ctx: Context, key: string): Promise<SsmParameterInterface | null>;
-  put(ctx: Context, key: string, value: SsmParameterInterface): Promise<SsmParameterInterface | null>;
+  put(
+    ctx: Context,
+    key: string,
+    value: SsmParameterInterface
+  ): Promise<SsmParameterInterface | null>;
   describe(ctx: Context, key: string): Promise<SsmParameterInterface | null>;
   delete(ctx: Context, key: string): Promise<boolean>;
 }
@@ -28,53 +32,64 @@ export class SsmService implements SsmServiceInterface {
 
     const values = await this.dataStore.getRoot<object>(ctx);
 
-    if(!values){
+    if (!values) {
       throw new Error("Datastore has no root element");
     }
 
     return Object.values(values);
   }
 
-  public async get(ctx: Context, key: string): Promise<SsmParameterInterface | null> {
+  public async get(
+    ctx: Context,
+    key: string
+  ): Promise<SsmParameterInterface | null> {
     ctx.logger.debug("SsmService.get");
 
     return await this.dataStore.get<SsmParameterInterface>(ctx, key);
   }
 
-  public async put(ctx: Context, key: string, value: SsmParameterInterface): Promise<SsmParameterInterface | null> {
+  public async put(
+    ctx: Context,
+    key: string,
+    value: SsmParameterInterface
+  ): Promise<SsmParameterInterface | null> {
     ctx.logger.debug("SsmService.put");
-    console.log({value});
+    console.log({ value });
 
     await this.dataStore.set<SsmParameterInterface>(ctx, key, value);
-    
+
     return await this.get(ctx, key);
   }
 
-  public async describe(ctx: Context, key: string): Promise<SsmParameterInterface | null> {
+  public async describe(
+    ctx: Context,
+    key: string
+  ): Promise<SsmParameterInterface | null> {
     ctx.logger.debug("SsmService.describe");
-    
-    try{
-      const values = await this.get(ctx, key);
-      console.log({values});
 
+    try {
+      const values = await this.get(ctx, key);
+      console.log({ values });
+
+      // TODO: I don't know what I should do here
       // TODO: return SsmParameterInterface|SsmParameterInterface[]
 
       return null;
-    }catch{
+    } catch {
       return null;
     }
   }
 
   public async delete(ctx: Context, key: string): Promise<boolean> {
     ctx.logger.debug("SsmService.delete");
-    
-    try{
-      if(await Promise.resolve(this.get(ctx, key)) !== null){
+
+    try {
+      if ((await Promise.resolve(this.get(ctx, key))) !== null) {
         await this.dataStore.delete(ctx, key);
 
-        return true;
+        return (await this.dataStore.get(ctx, key)) !== null;
       }
-    }catch{
+    } catch {
       // do nothing
     }
 
